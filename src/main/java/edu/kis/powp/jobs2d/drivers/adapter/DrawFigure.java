f3d16a525df978566efcfade6a3781e3417776ac
tobiashiro <t.felczynski@gmail.com>
2020-12-05 00:59:09 +0100

    zadanie2

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawFigure.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawFigure.java
@@ -4,15 +4,18 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
 public class DrawFigure extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
+	DrawPanelController drawPanelController;
 
 	public DrawFigure() {
 		super();
+		drawPanelController = DrawerFeature.getDrawerController();
 	}
 
 	@Override
@@ -27,8 +30,8 @@ public class DrawFigure extends DrawPanelController implements Job2dDriver {
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-
-		drawLine(line);
+		drawPanelController.drawLine(line);
+		//drawLine(line);
 		setPosition(x, y);
 	}
 
--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -10,6 +10,7 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.adapter.DrawFigure;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
+import edu.kis.powp.jobs2d.events.SelectDrawerFeature;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
 import edu.kis.powp.jobs2d.features.DriverFeature;
@@ -25,8 +26,11 @@ public class TestJobs2dPatterns {
 	private static void setupPresetTests(Application application) {
 		SelectTestFigureOptionListener selectTestFigureOptionListener = new SelectTestFigureOptionListener(
 				DriverFeature.getDriverManager());
+		SelectDrawerFeature selectDrawerFeature = new SelectDrawerFeature(
+				DriverFeature.getDriverManager());
 
 		application.addTest("Figure Joe 1", selectTestFigureOptionListener);
+		application.addTest("Figure Joe 2", selectDrawerFeature);
 	}
 
 	/**
