27411257943418766f3b22aa0088a5ec0c326bc3
Michal <michalsmiechowicz1@gmail.com>
2020-12-04 22:53:53 +0100

    Fixing display in wrong frame

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/ScreenDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/ScreenDrawerAdapter.java
@@ -4,13 +4,15 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
  */
-public class ScreenDrawerAdapter extends DrawPanelController implements Job2dDriver {
+public class ScreenDrawerAdapter implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
+	DrawPanelController drawPanelController = DrawerFeature.getDrawerController();
 	public ScreenDrawerAdapter() {
 		super();
 	}
@@ -26,7 +28,7 @@ public class ScreenDrawerAdapter extends DrawPanelController implements Job2dDri
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-		drawLine(line);
+		drawPanelController.drawLine(line);
 		setPosition(x, y);
 	}
 
