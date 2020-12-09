5a3a1d8b7f13ad6efc061efe495327d94ae1ac27
xawhub <ksawery.szwankowski@gmail.com>
2020-12-09 21:12:36 +0100

    Zad 3.3:1,2

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -9,6 +9,7 @@ import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
 import edu.kis.powp.jobs2d.drivers.adapter.DrawDriverAdapter;
+import edu.kis.powp.jobs2d.drivers.adapter.LineDrawerDriverAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListSecond;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
@@ -47,6 +48,9 @@ public class TestJobs2dPatterns {
 
 		Job2dDriver testDriver = new DrawDriverAdapter(DrawerFeature.getDrawerController());
 		DriverFeature.addDriver("Buggy Simulator", testDriver);
+
+		Job2dDriver secondTestDriver = new LineDrawerDriverAdapter(DrawerFeature.getDrawerController());
+		DriverFeature.addDriver("Special Line Simulator", secondTestDriver);
 
 		DriverFeature.updateDriverInfo();
 	}
