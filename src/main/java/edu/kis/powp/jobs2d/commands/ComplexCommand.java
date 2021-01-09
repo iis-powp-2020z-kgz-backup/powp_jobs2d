d49db20867976db37b0fca932ebd3be9a1d1ada5
s218156 <218156@edu.p.lodz.pl>
2021-01-09 15:06:55 +0100

    Poprawki wizualne

--- src/main/java/edu/kis/powp/jobs2d/commands/ComplexCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/commands/ComplexCommand.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ComplexCommand implements DriverCommand {
+
     private List<DriverCommand> commandList = new ArrayList<>();
+
     public ComplexCommand(List<DriverCommand> commandList) {
         this.commandList = commandList;
     }
+
     @Override
     public void execute() {
         commandList.forEach(e -> {
--- src/main/java/edu/kis/powp/jobs2d/commands/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/commands/SetPositionCommand.java
@@ -3,8 +3,10 @@ package edu.kis.powp.jobs2d.commands;
 import edu.kis.powp.jobs2d.Job2dDriver;
 
 public class SetPositionCommand implements DriverCommand {
+
     private int x;
     private int y;
+
     private Job2dDriver driver;
 
     public SetPositionCommand(Job2dDriver d, int a, int b){
@@ -12,12 +14,15 @@ public class SetPositionCommand implements DriverCommand {
         x = a;
         y = b;
     }
+
     public int getX() {
         return x;
     }
+
     public int getY() {
         return y;
     }
+
     @Override
     public void execute() {
         driver.setPosition(x,y);
