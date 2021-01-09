291be2a0d75711087373220049125112fc0d8571
s218156 <218156@edu.p.lodz.pl>
2021-01-09 15:02:08 +0100

    zadanie 3.1.2 i 3.1.3

---
+++ src/main/java/edu/kis/powp/jobs2d/commands/ComplexCommand.java
@@ -0,0 +1,17 @@
+package edu.kis.powp.jobs2d.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand {
+    private List<DriverCommand> commandList = new ArrayList<>();
+    public ComplexCommand(List<DriverCommand> commandList) {
+        this.commandList = commandList;
+    }
+    @Override
+    public void execute() {
+        commandList.forEach(e -> {
+            e.execute();
+        });
+    }
+}
