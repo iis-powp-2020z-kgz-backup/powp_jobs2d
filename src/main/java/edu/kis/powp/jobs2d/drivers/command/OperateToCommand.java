bbaa249a143d6b240ed3c7678463215ec2cfaf86
Adrian Panas <arszonto@gmail.com>
2021-01-16 01:10:05 +0100

    Add DriverCommand interface and basic commands: OperateTo, SetPosition

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
@@ -0,0 +1,20 @@
+package edu.kis.powp.jobs2d.drivers.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class OperateToCommand implements DriverCommand {
+    OperateToCommand(Job2dDriver nDriverContext, int n_x, int n_y) {
+        driverContext = nDriverContext;
+        x = n_x;
+        y = n_y;
+    }
+
+    @Override
+    public void execute() {
+        driverContext.operateTo(x,y);
+    }
+
+    private int x;
+    private int y;
+    private Job2dDriver driverContext;
+}
